517a3d81d7c1cc1bd7cd09d918504e4d72769420
thejas996@gmail.com <thejas996@gmail.com>
2019-03-07 16:25:01 +0530

    changes

--- AuctionMania/index.php
+++ AuctionMania/index.php
@@ -11,7 +11,7 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>AUCTIONMANIA</title>
+    <title>AUCTIONMANI A</title>
     <meta charset="utf-8">
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Your description">
